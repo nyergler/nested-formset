4453ad4a39e528b30a54b028e1194b411132cbad
Nathan Yergler <nathan@yergler.net>
2013-09-02 17:16:09 -0700

    Test parent-child formset instantiation.

---
+++ src/nested_formset/tests/test_instantiation.py
@@ -0,0 +1,30 @@
+from unittest import TestCase
+
+from django.forms.models import BaseInlineFormSet
+from nested_formset.tests import models as test_models
+
+from nested_formset import nested_formset_factory
+
+
+class InstantiationTests(TestCase):
+
+    def setUp(self):
+
+        self.formset_class = nested_formset_factory(
+            test_models.Block,
+            test_models.Building,
+            test_models.Tenant,
+        )
+
+    def test_takes_parent_discovers_children(self):
+
+        block = test_models.Block.objects.create()
+
+        for i in range(10):
+            test_models.Building.objects.create(block=block)
+
+        formset = self.formset_class(instance=block)
+
+        self.assertEqual(
+            len(formset.forms), 10 + formset.extra
+        )
