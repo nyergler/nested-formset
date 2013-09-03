28e5e09bed4db77e1699a0938dacb7a634b86772
Nathan Yergler <nathan@yergler.net>
2013-09-03 09:05:00 -0700

    Test creating both child and linked grandchildren at the same time.

--- src/nested_formset/tests/test_save.py
+++ src/nested_formset/tests/test_save.py
@@ -148,3 +148,39 @@ class CreationTests(TestCase):
             building.tenant_set.all()[0].name,
             'John Doe',
         )
+
+    def test_create_building_tenant(self):
+
+        self.assertEqual(self.block.building_set.count(), 0)
+
+        form_data = get_form_data(
+            self.formset_class(instance=self.block)
+        )
+        form_data.update({
+            'building_set-0-address': '829 S Mulberry St.',
+            'building_set-0-tenant_set-0-name': 'John Doe',
+            'building_set-0-tenant_set-0-unit': '42A',
+        })
+
+        form = self.formset_class(
+            instance=self.block,
+            data=form_data,
+        )
+
+        self.assertTrue(form.is_valid())
+        form.save()
+
+        # the building was created and linked to the block
+        self.assertEqual(self.block.building_set.count(), 1)
+        building = self.block.building_set.all()[0]
+        self.assertEqual(
+            building.address,
+            '829 S Mulberry St.',
+        )
+
+        # the tenant was also created and linked to the new building
+        self.assertEqual(building.tenant_set.count(), 1)
+        self.assertEqual(
+            building.tenant_set.all()[0].name,
+            'John Doe',
+        )
