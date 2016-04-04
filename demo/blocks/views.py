1abd727c0875be1b9c4bd0396f682623ee3d3e79
Narendra Choudhary <n.choudhary@iitg.ac.in>
2016-04-04 22:45:00 +0530

    Added fields attribute to class-based-views
    Django 1.8 makes it compulsory to set either `fields` attribute or `exclude` attribute to indicate which fields in the model should be used. Prior to 1.8, Django rendered all fields if none of `fields` or `exclude` attributes were mentioned.

--- demo/blocks/views.py
+++ demo/blocks/views.py
@@ -14,19 +14,24 @@ from blocks import models
 class ListBlocksView(ListView):
 
     model = models.Block
+    fields = '__all__'
 
 
 class CreateBlockView(CreateView):
+    
     model = models.Block
-
+    fields = '__all__'
+    
     def get_success_url(self):
 
         return reverse('blocks-list')
 
 
 class EditBuildingsView(UpdateView):
+    
     model = models.Block
-
+    fields = '__all__'
+    
     def get_template_names(self):
 
         return ['blocks/building_form.html']
@@ -38,7 +43,8 @@ class EditBuildingsView(UpdateView):
             models.Building,
             nested_formset=inlineformset_factory(
                 models.Building,
-                models.Tenant
+                models.Tenant,
+                fields = '__all__'
             )
         )
 
