2a2250445b8d947da38677620ac909ea982a2c36
Jon Dufresne <jon.dufresne@gmail.com>
2017-05-26 07:36:05 -0700

    Add testing for all supported Python and Django versions
    Now include Django 1.11 and Python 3.6 in testing.
    
    Moved to an explicit matrix as there isn't consistent overlap in support
    of Python across Django versions.
    
    Added all versions to the trove classifiers in setup.py.
    
    Added pip caching to travis defintion.

--- src/nested_formset/test_settings.py
+++ src/nested_formset/test_settings.py
@@ -117,11 +117,6 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'rebar.urls'
-
-# Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'rebar.wsgi.application'
-
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
